4fe01dae8c58d0acdc6e178593e8a2f10d44a437
Wei Wei Zhang <WWEZ@NNE.COM>
2018-08-03 10:41:35 +0800

    6-2

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Input, Button, List } from 'antd'
-class TodoListUI extends Component {
-    render() {
-        return (
-            <div style={{padding: 20}}>
-                <div style={{marginBottom: 10}}>
-                    <Input 
-                        placeholder='to do info' 
-                        onChange={this.props.handleInputChange} 
-                        value={this.props.inputValue}  
-                        style={{width: 300, marginRight: 10}} />
-                    <Button type='primary' onClick={this.props.handleBtnClick}>提交</Button>
-                </div>
-                <List 
-                    style={{width: 300}}
-                    bordered
-                    dataSource={this.props.list}
-                    renderItem={(item, index) => (<List.Item onClick={(index) => { this.props.handleItemDel(index) }}>{item}</List.Item>)}
-                />
+
+const TodoListUI = (props) => {
+    return (
+        <div style={{padding: 20}}>
+            <div style={{marginBottom: 10}}>
+                <Input 
+                    placeholder='to do info' 
+                    onChange={props.handleInputChange} 
+                    value={props.inputValue}  
+                    style={{width: 300, marginRight: 10}} />
+                <Button type='primary' onClick={props.handleBtnClick}>提交</Button>
             </div>
-        )
-    }
+            <List 
+                style={{width: 300}}
+                bordered
+                dataSource={props.list}
+                renderItem={(item, index) => (<List.Item onClick={(index) => { props.handleItemDel(index) }}>{item}</List.Item>)}
+            />
+        </div>
+    )
 }
 
 export default TodoListUI
